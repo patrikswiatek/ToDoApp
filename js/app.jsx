2f8cdf00b71640ea6e3423f16f5131635cbb1055
Patryk Swiatek <patrykswiatek@Air-Patryk.home>
2018-07-12 20:58:07 +0200

    Changes - stage 2

--- js/app.jsx
+++ js/app.jsx
@@ -3,40 +3,43 @@ import ReactDOM from 'react-dom';
 import {HashRouter} from "react-router-dom";
 import Route from "react-router-dom/es/Route";
 import Switch from "react-router-dom/es/Switch";
+import TodoList from './list.jsx'
 import './../main.scss';
 
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	class NotFound extends React.Component {
 
-		render() {
+		class NotFound extends React.Component {
 
-			return (
+			render() {
 
-				<div>
-					<h1>404</h1>
-				</div>
+				return (
 
-			);
+					<div>
+						<h1>404</h1>
+					</div>
+
+				);
+			}
 		}
-	}
 
-	class App extends React.Component {
+		class App extends React.Component {
 
-		render() {
+			render() {
 
-			return (<HashRouter>
-				<Switch>
-
-					<Route component={NotFound}/>
-				</Switch>
-			</HashRouter>)
+				return (<HashRouter>
+					<Switch>
+						<Route exact path='/' component={TodoList}/>
+						<Route component={NotFound}/>
+					</Switch>
+				</HashRouter>)
+			}
 		}
-	}
 
-	ReactDOM.render(<App/>, document.getElementById('app'));
-});
+		ReactDOM.render(<App/>, document.getElementById('app'));
+	});
+
 
 
 
--- js/list.jsx
+++ js/list.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
+import TodoItems from './items.jsx';
 
 
-class List extends React.Component {
+class TodoList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: []
+		};
+
+		this.addItem = this.addItem.bind(this);
+	}
+
+	addItem(e) {
+		if (this._inputElement.value !== "") {
+			let newItem = {
+				text: this._inputElement.value,
+				key: Date.now()
+			};
+
+			this.setState((prevState) => {
+				return {
+					items: prevState.items.concat(newItem)
+				};
+			});
+
+			this._inputElement.value = "";
+		}
+
+		console.log(this.state.items);
+
+		e.preventDefault();
+	}
+
 	render() {
-		let List = props => <ul>
-			{props.items.map((item, index) => <li key={index}>{item}</li>)}
-		</ul>;
+		return (
+			<div className="todoListMain">
+				<div className="header">
+					<form onSubmit={this.addItem}>
+
+						<input ref={(a) => this._inputElement = a}  placeholder="enter task">
+						</input>
+						<button type="submit">add</button>
+					</form>
+				</div>
+				<TodoItems entries={this.state.items}/>
+			</div>
+		);
 	}
 }
 
-export default List;
-
-
-
+export default TodoList;
